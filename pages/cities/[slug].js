4b45ca2ea94d24e8bb45f6575522493dd2813ba8
Markus <markus.sibbe@t-online.de>
2024-03-08 09:25:41 +0100

    redirection fix

--- pages/cities/[slug].js
+++ pages/cities/[slug].js
@@ -1,39 +1,42 @@
 import { cities } from "@/lib/data";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function CityPage() {
   const router = useRouter();
+  const { slug } = router.query;
+  const [city, setCity] = useState(null);
 
   function returnToCities() {
     router.push("/cities");
   }
 
-  const { slug } = router.query;
-  const city = cities.find((city) => city.slug === slug);
-
-  console.log("city", city);
-
-  if (!city) {
-    console.log("router", router);
-
-    //Wirft fehler
-    router.push("/cities");
-
-    return (
-      <>
-        <p>City not found</p>
-        <button onClick={returnToCities}>Return to cities page</button>
-      </>
-    );
-  }
+  useEffect(() => {
+    if (slug) {
+      const foundCity = cities.find((city) => city.slug === slug);
+      if (foundCity) {
+        setCity(foundCity);
+      } else {
+        router.push("/cities");
+      }
+    }
+  }, [slug]);
 
   return (
     <>
-      <h1>{city.name}</h1>
-      <p>Country: {city.country}</p>
-      <p>Population: {city.population}</p>
-      <p>Description: {city.description}</p>
-      <button onClick={returnToCities}>Return to cities page</button>
+      {city ? (
+        <>
+          <h1>{city.name}</h1>
+          <p>Country: {city.country}</p>
+          <p>Population: {city.population}</p>
+          <p>Description: {city.description}</p>
+          <button onClick={returnToCities}>Return to cities page</button>
+        </>
+      ) : (
+        <>
+          <p>Loading</p>
+        </>
+      )}
     </>
   );
 }
